8ea69ad5894386ac84a3444499292ad0c99e0224
PetkoMarinov <p.m.marinov@abv.bg>
2021-02-08 07:11:22 +0200

    new tasks
    Methods

---
+++ MethodsExercise/CharactersInRange/CharactersInRange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class CharactersInRange
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            
+            PrintCharactersInRange(start, end);
+        }
+
+        static void PrintCharactersInRange(char start, char end)
+        {
+            for (char i = start; i < end; i++)
+            {
+                if (i==start)
+                {
+                    continue;
+                }
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ MethodsExercise/MethodsExercise/VowelsCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class VowelsCount
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(VowelsCounter(input));
+        }
+
+        static int VowelsCounter(string input)
+        {
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (char.ToLower(input[i]))
+            {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'u':
+                    case 'i':
+                        counter++;break;
+                    default: 
+                        break;
+                }
+            }
+            return counter;
+        }
+    }
+}
