ca53736188e98da87d65ac795c4a2ed8b750897c
PetkoMarinov <p.m.marinov@abv.bg>
2021-02-12 07:12:26 +0200

    Lists
    adding new tasks

---
+++ ListsExercise/ListsExercise/Train.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsExercise
+{
+    class Train
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            while (command != "End".ToLower())
+            {
+                if (command.ToLower().Contains("add"))
+                {
+                    wagons.Add(int.Parse(command.Substring(command.IndexOf(" "))));
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (int.Parse(command) > capacity - wagons[i])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            wagons[i] = wagons[i] + int.Parse(command);
+                            i -= 1;
+                            break;
+                        }
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ MethodsMoreExercise/2. Center Point/CenterPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Center_Point
+{
+    class CenterPoint
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            PrintClosestPoint(x1, y1, x2, y2);
+        }
+
+        static void PrintClosestPoint(double x1, double y1, double x2, double y2)
+        {
+            double diagonalOne = Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+            double diagonalTwo = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+            if (diagonalOne <= diagonalTwo)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}
