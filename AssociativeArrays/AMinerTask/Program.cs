0f9ca05c7ffa9f72dfec0838a7a4b9cf267f718b
PetkoMarinov <p.m.marinov@abv.bg>
2021-03-12 22:44:24 +0200

    Associative Arrays - Exercise

---
+++ AssociativeArrays/AMinerTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            while (input!="stop")
+            {
+                string source = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (result.ContainsKey(source))
+                {
+                    result[source] += quantity;
+                }
+                else
+                {
+                    result[source] = quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ AssociativeArrays/CountCharsInString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+
+            foreach (var item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (countChars.ContainsKey(item[i]))
+                    {
+                        countChars[item[i]] += 1;
+                    }
+                    else
+                    {
+                        countChars.Add(item[i], 1);
+                    }
+                }
+            }
+
+            foreach (var item in countChars)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
