03dc5041579a44c79ec1902d192bd9ff5e50a22a
PetkoMarinov <p.m.marinov@abv.bg>
2021-03-23 23:40:31 +0200

    Regex

---
+++ RegexLab/MatchFullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(text, pattern);
+
+            Console.Write(string.Join(" ", matchedNames));
+
+        }
+    }
+}
---
+++ RegexLab/MatchPhoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359(?: |-)2(?: |-)\d{3}(?: |-)\d{4}";
+
+            string text = Console.ReadLine();
+
+            MatchCollection phoneNumber = Regex.Matches(text, pattern);
+
+            var matchedPhoneNums = phoneNumber
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ",matchedPhoneNums));
+        }
+    }
+}
