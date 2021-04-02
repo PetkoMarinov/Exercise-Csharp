ef9ccd46a97cb2c9873b3f42e3ece16945743e12
PetkoMarinov <p.m.marinov@abv.bg>
2021-04-03 00:21:35 +0300

    ExamPreparation

---
+++ ExamPreparation/P!rates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P_rates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sailInput = Console.ReadLine();
+
+            while (sailInput!="Sail")
+            {
+
+            }
+        }
+    }
+}
