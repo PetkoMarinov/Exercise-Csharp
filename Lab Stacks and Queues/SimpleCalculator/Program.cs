5d3c2e9cb8453696b5a3bdf4bf4827228a9a9154
PetkoMarinov <p.m.marinov@abv.bg>
2021-04-08 00:16:25 +0300

    LabStacksAndQueues

---
+++ Lab Stacks and Queues/SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []input = Console.ReadLine().Split(' ').Reverse().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+
+            while (stack.Count>1)
+            {
+               // Console.WriteLine(string.Join(" ",stack));
+                int first = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                switch (sign)
+                {
+                    case "+":stack.Push((first + second).ToString());break;
+                    case "-": stack.Push((first - second).ToString()); break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Lab Stacks and Queues/StackSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string input = Console.ReadLine().ToLower();
+
+            while (input != "end")
+            {
+                string command = input.Split()[0];
+                if (command == "add")
+                {
+                    string first = input.Split()[1];
+                    string second = input.Split()[2];
+                    stack.Push(int.Parse(first));
+                    stack.Push(int.Parse(second));
+                }
+                else if (command == "remove")
+                {
+                    int removedNums = int.Parse(input.Split()[1]);
+
+                    if (stack.Count >= removedNums)
+                    {
+                        for (int i = 0; i < removedNums; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
