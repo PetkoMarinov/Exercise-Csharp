d279821ee79d1df0f45939d57c9d1542e63e8d5d
PetkoMarinov <p.m.marinov@abv.bg>
2021-04-20 00:26:37 +0300

    Lab Stacks and Queues

---
+++ Lab Stacks and Queues/PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                int num = queue.Dequeue();
+                if (num % 2 == 0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+        }
+    }
+}
--- Lab Stacks and Queues/SimpleCalculator/Program.cs
+++ Lab Stacks and Queues/SimpleCalculator/Program.cs
@@ -8,19 +8,19 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            string []input = Console.ReadLine().Split(' ').Reverse().ToArray();
+            string[] input = Console.ReadLine().Split(' ').Reverse().ToArray();
             Stack<string> stack = new Stack<string>(input);
 
-            while (stack.Count>1)
+            while (stack.Count > 1)
             {
-               // Console.WriteLine(string.Join(" ",stack));
+                // Console.WriteLine(string.Join(" ",stack));
                 int first = int.Parse(stack.Pop());
                 string sign = stack.Pop();
                 int second = int.Parse(stack.Pop());
 
                 switch (sign)
                 {
-                    case "+":stack.Push((first + second).ToString());break;
+                    case "+": stack.Push((first + second).ToString()); break;
                     case "-": stack.Push((first - second).ToString()); break;
                 }
             }
